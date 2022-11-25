7479e5ec24fb970752f15abfdc27237a1ef5332b
im <ism1124@naver.com>
2022-11-25 12:38:06 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import PrintMyName from './component/PrintMyName';
+import { useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [user,setUsers] = useState([]);
+  const getUsers = async()=>{
+    const {data} = await axios.get("http://192.168.0.14:5000/user");
+    setUsers(data);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <PrintMyName name="123"></PrintMyName>
-        <PrintMyName name="456"></PrintMyName>
+        <button onClick={getUsers}>getUsers</button>
+        {user.map((el)=>(
+          <PrintMyName name={el.name}></PrintMyName>
+        ))}
+        
       </header>
     </div>
   );
